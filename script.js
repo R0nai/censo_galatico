444b3d4dbb29ff18b0371a60656804af20ab5906
Ronai Melo <amzronai@gmail.com>
2024-04-08 12:20:55 -0300

    Update script and html for better UI

--- script.js
+++ script.js
@@ -47,10 +47,12 @@ async function searchPlanet() {
     let  searchWord = document.getElementById("searchInput").value;
     let res = await fetch(`https://swapi.dev/api/planets/?search=${searchWord}`);
     let {results} = await res.json();
-    document.getElementById("searchPlanetName").innerText = results[0]['name'];
-    document.getElementById("searchPlanetClimate").innerText = results[0]['climate'];
-    document.getElementById("searchPlanetPopulation").innerText = results[0]['population'];
-    document.getElementById("searchPlanetTerrain").innerText = results[0]['terrain'];
+    document.getElementById("planetName").innerText = results[0]['name'];
+    document.getElementById("planetClimate").innerText = results[0]['climate'];
+    document.getElementById("planetPopulation").innerText = results[0]['population'];
+    document.getElementById("planetTerrain").innerText = results[0]['terrain'];
+    removeElementsFromUl();
+    printResidents(results[0]['residents']);
 }
 
 async function printResidents(residents) {
